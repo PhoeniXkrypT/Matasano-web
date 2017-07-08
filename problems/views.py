357f234f5712921947afe5eef77fde8f679312c8
phoenix <phoenixkrypt@gmail.com>
2017-07-08 14:15:33 +0100

    Modified views for index page

--- problems/views.py
+++ problems/views.py
@@ -8,8 +8,15 @@ from .models import SetOneQuestions
 # Create your views here.
 
 def index(request):
-    problem_sets = ['Set 1', 'Set 2', 'Set 3', 'Set 4']
-    context = {'problem_sets' : problem_sets}
+    problem_sets = {'Set1': 'Set 1: Basics', \
+                    'Set2': 'Set 2: Block Crypto', \
+                    'Set3': 'Set 3: Block and Stream Crypto', \
+                    'Set4': 'Set 4: Stream Crypto and Randomness', \
+                    'Set5': 'Set 5: Diffie-Hellman and friends', \
+                    'Set6': 'Set 6: RSA and DSA', \
+                    'Set7': 'Set 7: Hashes', \
+                    'Set8': 'Set 8: Abstract Algebra'}
+    context = {'problem_sets' : sorted(problem_sets.items())}
     return render(request, 'problems/index.html', context)
 
 def set_one(request):
