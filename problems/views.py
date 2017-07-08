dc222739f09b4697ffcafac01c7590825f1e8aa6
phoenix <phoenixkrypt@gmail.com>
2017-07-08 12:50:20 +0100

    Added basic structure
    - Added .gitignore
    - Added problems app to settings
    - Added urls for main page
    - Added template for index page
    - Added views and urls for the problems app index page

--- problems/views.py
+++ problems/views.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    problem_sets = ['Set 1', 'Set 2', 'Set 3', 'Set 4']
+    context = {'problem_sets' : problem_sets}
+    return render(request, 'problems/index.html', context)
+
+
