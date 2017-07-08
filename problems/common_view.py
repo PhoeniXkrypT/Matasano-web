aadfb696bc15e6a474a1afb65294b543bcfaadbf
phoenix <phoenixkrypt@gmail.com>
2017-07-08 23:07:35 +0100

    Add forms.py with AnswerForm
    - Modify qa-template and common_view.py to use AnswerForm

--- problems/common_view.py
+++ problems/common_view.py
@@ -1,13 +1,17 @@
 
 from django.shortcuts import render
+from django.views import View
+
+from .forms import AnswerForm
 
 def qa_view(request, question, qurl):
     html_template = 'problems/qa-template.html'
+    answer_form = AnswerForm()
     if request.method == 'GET':
-        context = {'question': question, 'qurl': qurl, 'status': ''}
+        context = {'question': question, 'qurl': qurl, 'status': '', 'answer_form': answer_form}
         return render(request, html_template, context)
     elif request.method == 'POST':
-        context = {'question': question, 'qurl': qurl, 'status': 'Wrong answer'}
+        context = {'question': question, 'qurl': qurl, 'status': 'Wrong answer', 'answer_form': answer_form}
         if request.POST.get('answer', '') == question.answer:
             context['status'] = 'Correct answer'
         return render(request, html_template, context)
